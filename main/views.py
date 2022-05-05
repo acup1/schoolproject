5dff1814e42c5ba7d102d879baf391d717a20a5b
acup1 <georgiyegoriy@gmail.com>
2022-05-05 14:36:59 +0300

    ewrewrewr

--- main/views.py
+++
@@ -1,9 +0,0 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-
-
-def mainpage(request):
-    return render(request,"main/main.html")
-
-def pr(request):
-    return render(request,"main/pr.html")
